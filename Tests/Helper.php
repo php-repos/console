8161c44f29d89bfb99a9a5c51ca6f8a7d8adc607
Morteza Poussaneh <morteza@protonmail.com>
2025-06-23 00:08:29 +0200

    Use different extension than .php to prevent building the commands in the test directory before copying

--- Tests/Helper.php
+++ Tests/Helper.php
@@ -2,6 +2,7 @@
 
 use PhpRepos\FileManager\Path;
 use function PhpRepos\FileManager\Directories\delete_recursive;
+use function PhpRepos\FileManager\Directories\ls_all;
 use function PhpRepos\FileManager\Directories\preserve_copy_recursively;
 use function PhpRepos\FileManager\Paths\realpath;
 
@@ -9,6 +10,11 @@ function copy_commands() {
     $helper_commands = realpath(__DIR__ . '/HelperCommands');
     $commands_directory = realpath(__DIR__ . '/../Commands');
     preserve_copy_recursively($helper_commands, $commands_directory);
+    foreach (ls_all($commands_directory) as $path) {
+        if (is_file($path) && str_ends_with($path, '.phpsample')) {
+            rename($path, str_replace('.phpsample', '.php', $path));
+        }
+    }
 }
 
 function delete_commands() {
